c080da28ee1a8ead6a9cbe3946fdc409dbc8dd02
Murillo Muller <murillo@meetsquad.ai>
2025-10-23 17:47:13 -0300

    Remove redundant prepare script and fix build script to use npx babel

--- build.sh
+++ build.sh
@@ -11,7 +11,7 @@ cp -r index.js index.d.ts lib dist/esm
 cp package.json dist/esm/package.json
 
 # build cjs - use babel to convert to commonjs
-babel index.js --out-file dist/cjs/index.js
-babel lib --out-dir dist/cjs/lib
+npx babel index.js --out-file dist/cjs/index.js
+npx babel lib --out-dir dist/cjs/lib
 cp index.d.ts dist/cjs/index.d.ts
 cp package.cjs.json dist/cjs/package.json
